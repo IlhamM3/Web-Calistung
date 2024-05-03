78fedb9c1fc3a4f7717aa5ae00cce9e7f28abbcc
ilham05 <ilhammaulana05303005@gmail.com>
2024-05-03 20:56:39 +0700

    fixed bug

--- src/stores/DataMembaca.js
+++ src/stores/DataMembaca.js
@@ -2,51 +2,51 @@ const DataMembaca = {
     imageShown: false,
     data: [
         {
-            "image": "@/assets/img/api.jpg",
+            "image": "../public/api.jpg",
             "caption": ["a", "pi"]
         },
         {
-            "image": "@/assets/img/baju.jpg",
+            "image": "../public/baju.jpg",
             "caption": ["ba", "ju"]
         },
         {
-            "image": "@/assets/img/baso.jpg",
+            "image": "../public/baso.jpg",
             "caption": ["ba", "so"]
         },
         {
-            "image": "@/assets/img/buku.jpg",
+            "image": "../public/buku.jpg",
             "caption": ["bu", "ku"]
         },
         {
-            "image": "@/assets/img/kopi.jpg",
+            "image": "../public/kopi.jpg",
             "caption": ["ko", "pi"]
         },
         {
-            "image": "@/assets/img/kuda.jpg",
+            "image": "../public/kuda.jpg",
             "caption": ["ku", "da"]
         },
         {
-            "image": "@/assets/img/matahari.jpg",
+            "image": "../public/matahari.jpg",
             "caption": ["ma", "ta", "ha", "ri"]
         },
         {
-            "image": "@/assets/img/petani.jpg",
+            "image": "../public/petani.jpg",
             "caption": ["pe", "ta", "ni"]
         },
         {
-            "image": "@/assets/img/rusa.jpg",
+            "image": "../public/rusa.jpg",
             "caption": ["ru", "sa"]
         },
         {
-            "image": "@/assets/img/sapi.jpg",
+            "image": "../public/sapi.jpg",
             "caption": ["sa", "pi"]
         },
         {
-            "image": "@/assets/img/susu.jpg",
+            "image": "../public/susu.jpg",
             "caption": ["su", "su"]
         },
         {
-            "image": "@/assets/img/topi.jpg",
+            "image": "../public/topi.jpg",
             "caption": ["to", "pi"]
         },
     ]
