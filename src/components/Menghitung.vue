a032e7178651d76814b28f122d440bc6b6893037
ilham05 <ilhammaulana05303005@gmail.com>
2024-05-04 00:44:36 +0700

    change

--- src/components/Menghitung.vue
+++ src/components/Menghitung.vue
@@ -58,7 +58,7 @@
                             from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4
                             focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg
                             text-sm px-5 py-2.5 text-center me-2 mb-2">
-                        Check Answer
+                        Kirim
                     </button>
                 </div>
                 <div v-if="message" class="fixed transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
